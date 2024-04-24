fc55aa7e8e68ab042755d84c58e117da5c7902d8
RevanQasimov23 <you@example.com>
2024-04-24 21:04:21 +0400

    first

---
+++ src/Components/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='menu'>
+
+        <div className='navbar'>
+            <ul className=''> 
+            <h1>Logo</h1> 
+            <li>Home</li>
+            <li>Agent</li>
+            <li>About</li>
+            <li>Listing</li>
+            <li>Blog</li>
+            <li>Contact</li>
+            </ul>
+        </div>
+
+<div className='img'>
+<h1>let us take you <br></br> further than <br></br> you've ever been</h1>
+</div> 
+
+<div className='sonluq'>
+  <div className='logo'>
+    <h1>Logo</h1>
+    <p>C 2022 Monkigo.All rights reserved.</p>
+    </div>
+
+<div className='subscribe'>
+  <h1>Subscribe</h1>
+  <p>Be the first one to hear about <br></br> new event and etc. </p>
+</div>
+
+</div>
+
+<div className='buton'>
+<button className='email'>Email addres</button>
+  <button className='subs'>Subscribe</button>
+</div>
+
+</div>
+
+    
+   
+
+
+
+  )
+}
